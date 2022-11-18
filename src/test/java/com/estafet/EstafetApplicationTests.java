95ef3eed880c344e3bf829bd5181c575586871c0
javid <cavid_xalilov@mail.ru>
2022-11-18 11:58:07 +0400

    restore DB

--- src/test/java/com/estafet/EstafetApplicationTests.java
+++ src/test/java/com/estafet/EstafetApplicationTests.java
@@ -1,10 +1,85 @@
 package com.estafet;
 
+import com.estafet.domain.Customer;
+import com.estafet.domain.Task;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.io.IOException;
+import java.util.List;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
+@WebAppConfiguration
 class EstafetApplicationTests {
+
+	@Autowired
+	protected MockMvc mvc;
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	protected String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+			throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
+
+	@Test
+	public void getTasks() throws Exception {
+		String uri = "/tasks";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		Task[] tasks = mapFromJson(content, Task[].class);
+		assertTrue(tasks.length > 0);
+	}
+
+	@Test
+	public void addTask() throws Exception {
+		String uri = "/add";
+		Task task = new Task(1L,"testTitle", "testDesc", false, new Customer("Test"));
+		String inputJson = mapToJson(task);
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+				.contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		assert (content.contains("id"));
+		assert (content.contains("title"));
+		assert (content.contains("description"));
+	}
 
 	@Test
 	void contextLoads() {
